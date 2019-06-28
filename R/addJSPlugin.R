ed497746452774bea82f3a0af4e781eeeee955db
Jason Klaassen <jwk24@students.calvin.edu>
2019-06-28 15:38:23 -0400

    Organized the vignettes, added examples, cleared all warnings and notes, fixed a coloring bug

--- R/addJSPlugin.R
+++ R/addJSPlugin.R
@@ -1,31 +1,35 @@
-# Some Code used and adapted from https://gist.github.com/jcheng5/c084a59717f18e947a17955007dc5f92
-
-# functional wrapper for htmlDependency constructor
-createPlugin <- function(name, version, src, script, stylesheet = NULL) {
-  htmlDependency(name, version, src = src, script = script, stylesheet = stylesheet)
-}
-
-# A function that takes a plugin htmlDependency object and adds
-# it to the map. This ensures that however or whenever the map
-# gets rendered, the plugin will be loaded into the browser.
-registerPlugin <- function(map, plugin) {
-  map$dependencies <- c(map$dependencies, list(plugin))
-  map
-}
-
 #' Create plugin functions to implement Leaflet pure-JS libraries in R
 #'
-#' @description pluginFactory takes in a couple js files (code and dependencies)
+#' @description `pluginFactory()` takes in a couple js files (code and dependencies)
 #' and creates a plugin function that applies this plugin to a given map. The new
 #' function also returns the map so it can be piped with the %>% operator like other
 #' leaflet functions
 #'
+#' @import leaflet htmltools htmlwidgets tibble
 #' @param name A name for your plugin
 #' @param location The parent folder of your js file, dependency file, and css stylesheet
 #' @param jsfilename A .js filename relative to the parent folder at `location` that holds the source code for modifying the map object
 #' @param dependencies A .js filename(s) of all other js libraries the source code depends on
 #' @param stylesheet A .css stylesheet for adding styles to the entire page
 #' @return A function with arguments map, data, and ... to be used as a plugin to leaflet just like the leaflet::addCircles() function
+#' @seealso Some code is used from [this github gist by Joe Chang](https://gist.github.com/jcheng5/c084a59717f18e947a17955007dc5f92)
+#' @examples
+#'
+#' df <- data.frame(
+#'   lat =  42.9634 + stats::rnorm(100),
+#'   lng = -85.6681 + stats::rnorm(100),
+#'   size = runif(100, 5, 20),
+#'   color = sample(colors(), 100)
+#' )
+#' addJS <-
+#'   pluginFactory(
+#'     "Some JS Plugin",
+#'     system.file("js", "", package = "leaflethex"), "hexbin.js", "deps.js", stylesheet="hexbin.css")
+#'
+#' leaflet(df, width = 600, height = 300) %>%
+#'   addTiles() %>%
+#'   addJS(radius = 20, highEndColor = "yellow")
+#'
 #' @export
 pluginFactory <- function(name = "JSPlugin", location, jsfilename, dependencies, stylesheet = NULL) {
 
@@ -54,8 +58,21 @@ pluginFactory <- function(name = "JSPlugin", location, jsfilename, dependencies,
     jscode <- readJSFileToString(location, jsfilename)
 
     # Load Dependencies and render JS Code with the data and extra parameters
-    registerPlugin(map, plugin) %>% onRender(jscode, data=dots)
+    registerPlugin(map, plugin) %>% htmlwidgets::onRender(jscode, data=dots)
   }
+}
+
+# functional wrapper for htmlDependency constructor
+createPlugin <- function(name, version, src, script, stylesheet = NULL) {
+  htmltools::htmlDependency(name, version, src = src, script = script, stylesheet = stylesheet)
+}
+
+# A function that takes a plugin htmlDependency object and adds
+# it to the map. This ensures that however or whenever the map
+# gets rendered, the plugin will be loaded into the browser.
+registerPlugin <- function(map, plugin) {
+  map$dependencies <- c(map$dependencies, list(plugin))
+  map
 }
 
 # Extra utility function to convert .js files to strings
--- R/hexbin.R
+++ R/hexbin.R
@@ -1,18 +1,27 @@
 #' Hexbin Layers for Leaflet Plots
 #'
-#' @description Create hexbin layers for leaflet plots.
-#'
-#' @param map The leaflet map object to apply the hexbin layer to. Makes this function compatible with the %>% operator
+#' Create hexbin layers for leaflet plots.
+#' @importFrom dplyr %>%
+#' @import leaflet
+#' @param map The leaflet map object to apply the hexbin layer to. Makes this function compatible with the `%>%` operator
 #' @param data data frame or tibble - alternate data to use for this hexbin instead of default map data
 #' @param radius choose the base size for the hexagons
 #' @param opacity decimal between 0.0 and 1.0 - choose the percent of opacity for the hexagons
+#' @param duration I don't actually know what this does
 #' @param lowEndColor choose the color for the smaller hexagons
 #' @param highEndColor choose the color for the larger hexagons
 #' @param uniformSize boolean for having uniformly sized hexagons or smaller hexagons for area containing fewer data points
 #' @param uniformColor a color that overrides lowEndColor and highEndColor to make the color uniform across the hexagon sizes.
 #' @note Do not use uniformColor and uniformSize together as it will not give any insights to the data
+#' @seealso A [JSFiddler Hexbin example](https://jsfiddle.net/reblace/acjnbu8t/?utm_source=website&utm_medium=embed&utm_campaign=acjnbu8t) by Ryan
+#' @return map parameter, but with the hexbinLayer attached so that it can be used with the `%>%` pipe operator
 #'
-#' @return map parameter, but with the hexbinLayer attached so that it can be used with the %>% pipe operator
+#' @examples
+#' leaflet::leaflet(data.frame(lat =  42.9634 + rnorm(1000),lng = -85.6681 + rnorm(1000))) %>%
+#' addTiles() %>% add_hexbin()
+#'
+#' leaflet::leaflet(data.frame(lat =  42.9634 + rnorm(1000),lng = -85.6681 + rnorm(1000))) %>%
+#' addTiles() %>% add_hexbin(radius=25, lowEndColor='purple', highEndColor='orange')
 #' @export
 #'
 #'
@@ -29,7 +38,9 @@ add_hexbin <-
 
     # Build MapData from given data or mapData if none provided
     mapData <- if(!is.null(data)) data else leaflet::getMapData(map)
-
+    if(uniformSize && is.null(uniformColor)) {
+      warning("Using uniformSize and uniformColor together will not provide any insights into the data")
+    }
     # Ensure the data passed to the JS script is a JSON object
     class(mapData) <- "options"
 
